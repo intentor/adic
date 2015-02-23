4b9cb083a327fcb5772269b41cc52038e8697c98
André Intentor Martins <andre@intentor.com.br>
2015-02-23 14:42:44 -0300

    Added Binding Game Objects example.

---
+++ src/Assets/Adic/Examples/02_BindingGameObjects/Scripts/GameRoot.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Intentor.Adic;
+
+namespace Intentor.Adic.BindingGameObjects {
+	/// <summary>
+	/// Game context root.
+	/// </summary>
+	public class GameRoot : Intentor.Adic.ContextRoot {
+		public override void SetupContainers() {
+			//Creates the container.
+			var container = new InjectionContainer();
+			//Adds any extensions the container may use.
+			container.RegisterExtension<UnityBindingContainerExtension>();
+
+			//Binds a Transform component to the "Cube" GameObject.
+			container.Bind<Transform>().ToGameObject("Cube");
+			//Binds the "GameObjectRotator" component to a new GameObject of the same name.
+			//This component will then receive the reference above so it can rotate the cube.
+			container.Bind<GameObjectRotator>().ToGameObject();
+			
+			//Adds the container to the context.
+			this.AddContainer(container);
+		}
+		
+		public override void Init() {
+			//Init the game.
+		}
+	}
+}
