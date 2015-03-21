20b8d2a41386cf2eae3ecadedd62db71340356fe
André Intentor Martins <andre@intentor.com.br>
2015-03-21 14:17:17 -0300

    Added Bindings Setup extension.

---
+++ src/Assets/Adic/Scripts/Extensions/BindingsSetup/BindingsSetupExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Adic.Container;
+
+namespace Adic {
+	/// <summary>
+	/// Provides bindings setup capabilities to <see cref="Adic.Container.IInjectionContainer"/>.
+	/// </summary>
+	public static class BindingsSetupExtension {
+		/// <summary>
+		/// Setups bindings in the container.
+		/// </summary>
+		/// <typeparam name="T">The bindings setup object type.</typeparam>
+		/// <param name="container">Container in which the bindings will be setup.</param>
+		/// <param name="setup">The bindings setup.</param>
+		public static void SetupBindings<T>(this IInjectionContainer container) where T : IBindingsSetup, new() {
+			container.SetupBindings(typeof(T));
+		}
+
+		/// <summary>
+		/// Setups bindings in the container.
+		/// </summary>
+		/// <param name="container">Container in which the bindings will be setup.</param>
+		/// <param name="type">The bindings setup object type.</param>
+		public static void SetupBindings(this IInjectionContainer container, Type type) {
+			var setup = (IBindingsSetup)Activator.CreateInstance(type);
+			container.SetupBindings(setup);
+		}
+
+		/// <summary>
+		/// Setups bindings in the container.
+		/// </summary>
+		/// <param name="container">Container in which the bindings will be setup.</param>
+		/// <param name="setup">The bindings setup.</param>
+		public static void SetupBindings(this IInjectionContainer container, IBindingsSetup setup) {
+			setup.SetupBindings(container);
+		}
+	}
+}
---
+++ src/Assets/Adic/Scripts/Extensions/BindingsSetup/IBindingsSetup.cs
@@ -0,0 +1,14 @@
+﻿using Adic.Container;
+
+namespace Adic {
+	/// <summary>
+	/// Represents a bindings setup object.
+	/// </summary>
+	public interface IBindingsSetup {
+		/// <summary>
+		/// Setups the bindings in a given <paramref name="container"/>.
+		/// </summary>
+		/// <param name="container">Container in which the bindings will be setup.</param>
+		void SetupBindings(IInjectionContainer container);
+	}
+}
