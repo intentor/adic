29f75760ba24bff069c02138cd8ee8436e7dbf64
André Intentor Martins <andre@intentor.com.br>
2015-06-26 12:45:03 -0300

    Added MonoDevelop project for generating DLLs. Issue #14.

---
+++ bin/extensions/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Adic.Extensions")]
+[assembly: AssemblyDescription("Adic Extensions (Unity only)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Intentor")]
+[assembly: AssemblyProduct("Adic - Another Dependency Injection Container")]
+[assembly: AssemblyCopyright("2014-2015 Intentor - http://intentor.com.br/")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("2.14")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
