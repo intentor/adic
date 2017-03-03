f88df931b6bc3bfbe7ffd8bd87011c29cca963a9
André "Intentor" Martins <andre@intentor.com.br>
2017-03-02 21:46:38 -0300

    Add testbed scene

---
+++ src/Assets/Tests/Testbed/Scripts/GameRoot.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Adic;
+
+namespace Adic.Examples.Testbed {
+	/// <summary>
+	/// Game context root.
+	/// </summary>
+	public class GameRoot : ContextRoot {
+		public override void SetupContainers() {
+            var container = this.AddContainer<InjectionContainer>()
+                .RegisterExtension<UnityBindingContainerExtension>()
+                .Bind<TestBehaviour>().ToGameObject();
+		}
+		
+		public override void Init() {
+			//Init the game.
+		}
+	}
+}
---
+++ src/Assets/Tests/Testbed/Scripts/TestBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Adic.Examples.Testbed {
+	/// <summary>
+	/// Simple test behaviour.
+	/// </summary>
+    public class TestBehaviour : MonoBehaviour  {
+		public void Start() {
+            Debug.Log("TestBehaviour");
+		}
+	}
+}
