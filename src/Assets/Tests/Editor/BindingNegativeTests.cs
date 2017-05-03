ae294038f54e80a20ed937f945d4035f818d6272
André "Intentor" Martins <andre@intentor.com.br>
2017-05-02 21:54:14 -0300

    Update tests which expect exceptions

--- src/Assets/Tests/Editor/BindingNegativeTests.cs
+++ src/Assets/Tests/Editor/BindingNegativeTests.cs
@@ -9,69 +9,79 @@ namespace Adic.Tests {
     [TestFixture]
     public class BindingNegativeTests {
         [Test]
-        [ExpectedException(typeof(BinderException))]
         public void TestBindToInterfaceSelf() {
             var binder = new Binder();
-			
-            binder.Bind<IMockInterface>().ToSelf();
+
+            Assert.Throws(typeof(BinderException), delegate {
+                binder.Bind<IMockInterface>().ToSelf();
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BinderException))]
         public void TestBindToInterfaceSingleton() {
             var binder = new Binder();
-			
-            binder.Bind<IMockInterface>().ToSingleton();
+
+            Assert.Throws(typeof(BinderException), delegate {
+                binder.Bind<IMockInterface>().ToSingleton();
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BinderException))]
         public void TestBindToInterfaceTransient() {
             var binder = new Binder();
-			
-            binder.Bind<IMockInterface>().To<IMockInterface>();
+
+            Assert.Throws(typeof(BinderException), delegate {
+                binder.Bind<IMockInterface>().To<IMockInterface>();
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BindingException))]
         public void TestBindNotAssignableKeyTypeToSingleton() {
             var binder = new Binder();
-			
-            binder.Bind<IMockInterface>().ToSingleton<MockClassToDepend>();
+
+            Assert.Throws(typeof(BindingException), delegate {
+                binder.Bind<IMockInterface>().ToSingleton<MockClassToDepend>();
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BindingException))]
         public void TestBindNotAssignableKeyTypeToTransient() {
             var binder = new Binder();
-			
-            binder.Bind<IMockInterface>().To<MockClassToDepend>();
+
+            Assert.Throws(typeof(BindingException), delegate {
+                binder.Bind<IMockInterface>().To<MockClassToDepend>();
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BindingException))]
         public void TestBindNotAssignableKeyTypeToInstance() {
             var binder = new Binder();
 
             var instance = new MockClassToDepend();
-            binder.Bind<IMockInterface>().To<MockClassToDepend>(instance);
+
+            Assert.Throws(typeof(BindingException), delegate {
+                binder.Bind<IMockInterface>().To<MockClassToDepend>(instance);
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BindingException))]
         public void TestBindNotAssignableInstanceTypeToInstance() {
             var binder = new Binder();
 
             var instance = new MockClassToDepend();
-            binder.Bind<MockClassToDepend>().To(typeof(MockClassVerySimple), instance);
+
+            Assert.Throws(typeof(BindingException), delegate {
+                binder.Bind<MockClassToDepend>().To(typeof(MockClassVerySimple), instance);
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(BindingException))]
         public void TestBindFactoryToNotFactory() {
             var binder = new Binder();
 
-            binder.Bind<MockClassToDepend>().ToFactory(typeof(MockClassToDepend));
+            Assert.Throws(typeof(BindingException), delegate {
+                binder.Bind<MockClassToDepend>().ToFactory(typeof(MockClassToDepend));
+            });
         }
     }
 }
